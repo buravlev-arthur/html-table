1e0dafa0632fbe5c0dabb80d85c79bb3ae83c935
Arthur Buravlev <buravlev-arthur@yandex.ru>
2025-05-28 16:11:44 +0300

    created Table class, code refactoring

---
+++ src/table.class.ts
@@ -0,0 +1,193 @@
+import {Cell, Column} from "./types";
+
+export default class Table {
+  private readonly columnChars: string[] = [...'abcdefghijklmnopqrstuvwxyz'];
+  private readonly columnDefaultWidthPx: number = 80;
+  private readonly rowHeaderCellWidthPx = 63;
+  private readonly _tableEl = document.createElement("div");
+  private readonly _rowHeadersContainerElement = document.createElement("div");
+  private readonly _tableContentEl = document.createElement("div");
+
+  public readonly cellPaddingPx: number = 12;
+  public readonly tableHeaderHeightPx: number = 28;
+
+  private _cells = new Map<string, Cell>();
+  private _columns = new Map<number, Column>();
+  private _tableRowHeaderCellElements: HTMLDivElement[] = [];
+  private _tableRowElements: HTMLDivElement[] = [];
+
+  constructor(
+    private readonly container: HTMLDivElement,
+    private readonly availableRowsCount: number,
+    private readonly rowHeight: number,
+    private readonly visibleRowsCount: number,
+    private readonly rowsBuffer: number,
+  ) {
+    this.init();
+  }
+
+  private init(): void {
+    this._tableEl.classList.add("table");
+    this._tableEl.style.height = `${this.availableRowsCount * this.rowHeight}px`;
+    this.container.appendChild(this.tableEl);
+
+    this._rowHeadersContainerElement.classList.add("row-headers-container");
+    this.tableEl.appendChild(this.rowHeadersContainerElement);
+
+    this._tableContentEl.classList.add("table-content");
+    this._tableEl.appendChild(this.tableContentEl);
+
+    this.setColumns();
+    this.setCells();
+    this.createRowHTMLElements();
+  }
+
+  get tableRowHeaderCellElements() {
+    return this._tableRowHeaderCellElements;
+  }
+
+  get tableRowElements() {
+    return this._tableRowElements;
+  }
+
+  get cells() {
+    return this._cells;
+  }
+
+  get columns() {
+    return this._columns;
+  }
+
+  get tableEl() {
+    return this._tableEl;
+  }
+
+  get tableContentEl() {
+    return this._tableContentEl;
+  }
+
+  get rowHeadersContainerElement() {
+    return this._rowHeadersContainerElement;
+  }
+
+  private setColumns(): void {
+    const columnHeadersContainer = document.createElement("div");
+    columnHeadersContainer.classList.add("column-headers-container");
+
+    let cellsTotalWidth = this.rowHeaderCellWidthPx;
+    for (let columnIndex = 0; columnIndex < this.columnChars.length; columnIndex += 1) {
+      const title = this.columnChars[columnIndex];
+      const width = this.columnDefaultWidthPx;
+
+      this._columns.set(columnIndex, {
+        title,
+        width
+      });
+
+      const columnHeaderCellEl = document.createElement("div");
+      columnHeaderCellEl.classList.add("column-header-cell", "cell");
+      columnHeaderCellEl.style.width = `${width}px`;
+      columnHeaderCellEl.textContent = title;
+      columnHeaderCellEl.style.transform = `translateX(${cellsTotalWidth}px)`;
+
+      columnHeadersContainer.appendChild(columnHeaderCellEl);
+
+      cellsTotalWidth += width + this.cellPaddingPx;
+    }
+
+    columnHeadersContainer.style.width = `${cellsTotalWidth}px`;
+    this.tableEl.appendChild(columnHeadersContainer);
+  }
+
+  private setCells(): void {
+    for (let rowIndex = 0; rowIndex < this.availableRowsCount; rowIndex += 1) {
+      for (let columnIndex = 0; columnIndex < this._columns.size; columnIndex += 1) {
+        this._cells.set(`${rowIndex}_${columnIndex}`, {
+          formula: '',
+          calculatedValue: `${rowIndex}, ${columnIndex}`,
+          type: 'String',
+          formating: []
+        });
+      }
+    }
+  }
+
+  private createRowHTMLElements(): void {
+    const rowElementsCount = this.visibleRowsCount + this.rowsBuffer * 2;
+
+    for (let rowIndex = 0; rowIndex < rowElementsCount; rowIndex += 1) {
+      const rowEl = document.createElement("div");
+      rowEl.classList.add("row");
+      rowEl.setAttribute("data-row-index", `${rowIndex}`);
+      rowEl.setAttribute("data-row-array-index", `${rowIndex}`);
+      rowEl.style.height = `${this.rowHeight}px`;
+      rowEl.style.display = "none";
+
+      let previousCellsWidth = 0;
+
+      const rowHeaderCellElement = this.getRowHeaderCell(rowIndex, this.rowHeight);
+      this._tableRowHeaderCellElements.push(rowHeaderCellElement);
+      this.rowHeadersContainerElement.appendChild(rowHeaderCellElement);
+
+      this.columns.forEach((column, columnIndex) => {
+        const cellElement = this.getCellElement(rowIndex, column, columnIndex, previousCellsWidth);
+        rowEl.appendChild(cellElement);
+
+        previousCellsWidth += column.width + this.cellPaddingPx;
+      });
+
+      rowEl.style.width = `${previousCellsWidth}px`;
+
+      this._tableRowElements.push(rowEl);
+      this.tableContentEl.appendChild(rowEl);
+    }
+  }
+
+  private getRowHeaderCell(rowIndex: number, rowHeight: number): HTMLDivElement {
+    const rowHeaderCellElement = document.createElement("div");
+    rowHeaderCellElement.classList.add("cell");
+    rowHeaderCellElement.style.height = `${rowHeight}px`;
+    rowHeaderCellElement.textContent = String(rowIndex + 1);
+    rowHeaderCellElement.style.display = "none";
+
+    return rowHeaderCellElement;
+  }
+
+  private getCellElement(rowIndex: number, column: Column, columnIndex: number, previousCellsWidth: number): HTMLDivElement {
+    const cellElement = document.createElement("div");
+    cellElement.classList.add("cell");
+    cellElement.setAttribute('data-row', `${rowIndex}`);
+    cellElement.setAttribute('data-column', `${columnIndex}`);
+    cellElement.style.width = `${column.width}px`;
+    cellElement.style.transform = `translateX(${previousCellsWidth}px)`;
+    cellElement.textContent = this.cells.get(`${rowIndex}_${columnIndex}`)?.calculatedValue ?? '';
+
+    return cellElement;
+  }
+
+  public renderRow(rowIndex: number, rowContentBlockIndex: number, previousRowsHeight: number): void {
+    const rowHeaderCellElement = this.tableRowHeaderCellElements[rowContentBlockIndex];
+    rowHeaderCellElement.style.transform = `translateY(${previousRowsHeight + this.tableHeaderHeightPx}px)`;
+    rowHeaderCellElement.style.display = "flex";
+    rowHeaderCellElement.textContent = `${rowIndex + 1}`;
+
+    const tableRowEl = this.tableRowElements[rowContentBlockIndex];
+    tableRowEl.setAttribute("data-row-index", `${rowIndex}`);
+    tableRowEl.setAttribute("data-row-array-index", `${rowContentBlockIndex}`);
+    tableRowEl.style.transform = `translateY(${previousRowsHeight}px)`;
+    tableRowEl.style.display = 'flex';
+
+    tableRowEl.querySelectorAll('.cell').forEach((cellElement, cellIndex) => {
+      cellElement.setAttribute('data-row', `${rowIndex}`);
+      cellElement.textContent = this.cells.get(`${rowIndex}_${cellIndex}`)?.calculatedValue ?? '';
+    });
+  }
+
+
+  public hideRows(): void {
+    for (const [index, rowElement] of this.tableRowElements.entries()) {
+      rowElement.style.display = "none";
+      this.tableRowHeaderCellElements[index].style.display = "none";
+    }
+  }
+}
--- src/virtual-scroll.class.ts
+++ src/virtual-scroll.class.ts
@@ -1,14 +1,9 @@
-import {Cell, Column} from "./types";
+import Table from './table.class';
 
 export default class VirtualScroll {
-  private readonly columnDefaultWidth: number = 80;
-  private readonly tableHeaderHeightPx: number = 28;
-  private readonly columnChars: string[] = [...'abcdefghijklmnopqrstuvwxyz'];
-  private readonly cellPaddingPx: number = 12;
-
   private observer: IntersectionObserver | null = null;
-  private tableEl: HTMLDivElement | null = null;
-  private tableContentEl: HTMLDivElement | null = null;
+  private itemEl: HTMLDivElement | null = null;
+  private itemContentEl: HTMLDivElement | null = null;
   private visibleRowsCount: number = 0;
   private startIndex: number = 0;
   private endIndex: number = 0;
@@ -16,166 +11,49 @@ export default class VirtualScroll {
   private bottomSentinel: HTMLDivElement | null = null;
   private scrollHandler: (() => void) | null = null;
   private rowsBuffer = 10;
-  private tableRowElements: HTMLDivElement[] = [];
-  private tableRowHeaderCellElements: HTMLDivElement[] = [];
-  private columns = new Map<number, Column>();
-  private cells = new Map<string, Cell>();
-  private rowHeadersContainerElement: HTMLDivElement | null = null;
+  private table: Table | null = null;
 
   constructor(
     private readonly container: HTMLDivElement,
     private readonly availableRowsCount: number,
     private readonly rowHeight: number,
   ) {
-    this.container = container;
-    this.availableRowsCount = availableRowsCount;
-    this.rowHeight = rowHeight;
-
     this.init();
     this.addScrollListener();
   }
 
   private init(): void {
-    this.tableEl = document.createElement("div");
-    this.tableEl.classList.add("table");
-    this.tableEl.style.height = `${this.availableRowsCount * this.rowHeight}px`;
-    this.container.appendChild(this.tableEl);
-
-    this.rowHeadersContainerElement = document.createElement("div");
-    this.rowHeadersContainerElement.classList.add("row-headers-container");
-    this.tableEl.appendChild(this.rowHeadersContainerElement);
-
-    this.tableContentEl = document.createElement("div");
-    this.tableContentEl.classList.add("table-content");
-    this.tableEl.appendChild(this.tableContentEl);
-
     this.visibleRowsCount = Math.ceil(this.container.clientWidth / this.rowHeight);
 
-    this.setColumns();
-    this.setCells();
-    this.createRowHTMLElements();
+    this.table = new Table(
+      this.container,
+      this.availableRowsCount,
+      this.rowHeight,
+      this.visibleRowsCount,
+      this.rowHeight,
+    );
+
+    this.itemEl = this.table.tableEl;
+    this.itemContentEl = this.table.tableContentEl;
+
     this.createSentinelElements();
     this.renderVisibleRows();
   }
 
-  setColumns(): void {
-    if (!this.tableEl) {
-      return;
-    }
-
-    const rowHeaderCellWidthPx = 63;
-    const columnHeadersContainer = document.createElement("div");
-    columnHeadersContainer.classList.add("column-headers-container");
-
-    let cellsTotalWidth = rowHeaderCellWidthPx;
-    for (let columnIndex = 0; columnIndex < this.columnChars.length; columnIndex += 1) {
-      const title = this.columnChars[columnIndex];
-      const width = this.columnDefaultWidth;
-
-      this.columns.set(columnIndex, {
-        title,
-        width
-      });
-
-      const columnHeaderCellEl = document.createElement("div");
-      columnHeaderCellEl.classList.add("column-header-cell", "cell");
-      columnHeaderCellEl.style.width = `${width}px`;
-      columnHeaderCellEl.textContent = title;
-      columnHeaderCellEl.style.transform = `translateX(${cellsTotalWidth}px)`;
-
-      columnHeadersContainer.appendChild(columnHeaderCellEl);
-
-      cellsTotalWidth += width + this.cellPaddingPx;
-    }
-
-    columnHeadersContainer.style.width = `${cellsTotalWidth}px`;
-    this.tableEl.appendChild(columnHeadersContainer);
-  }
-
-  setCells(): void {
-    for (let rowIndex = 0; rowIndex < this.availableRowsCount; rowIndex += 1) {
-      for (let columnIndex = 0; columnIndex < this.columns.size; columnIndex += 1) {
-        this.cells.set(`${rowIndex}_${columnIndex}`, {
-          formula: '',
-          calculatedValue: `${rowIndex}, ${columnIndex}`,
-          type: 'String',
-          formating: []
-        });
-      }
-    }
-  }
-
-  private createRowHTMLElements(): void {
-    if (!this.tableContentEl || !this.rowHeadersContainerElement) {
-      return;
-    }
-
-    const rowElementsCount = this.visibleRowsCount + this.rowsBuffer * 2;
-
-    for (let rowIndex = 0; rowIndex < rowElementsCount; rowIndex += 1) {
-      const rowEl = document.createElement("div");
-      rowEl.classList.add("row");
-      rowEl.setAttribute("data-row-index", `${rowIndex}`);
-      rowEl.setAttribute("data-row-array-index", `${rowIndex}`);
-      rowEl.style.height = `${this.rowHeight}px`;
-      rowEl.style.display = "none";
-
-      let previousCellsWidth = 0;
-
-      const rowHeaderCellElement = this.getRowHeaderCell(rowIndex, this.rowHeight);
-      this.tableRowHeaderCellElements.push(rowHeaderCellElement);
-      this.rowHeadersContainerElement.appendChild(rowHeaderCellElement);
-
-      this.columns.forEach((column, columnIndex) => {
-        const cellElement = this.getCellElement(rowIndex, column, columnIndex, previousCellsWidth);
-        rowEl.appendChild(cellElement);
-
-        previousCellsWidth += column.width + this.cellPaddingPx;
-      });
-
-      rowEl.style.width = `${previousCellsWidth}px`;
-
-      this.tableRowElements.push(rowEl);
-      this.tableContentEl.appendChild(rowEl);
-    }
-  }
-
-  private getRowHeaderCell(rowIndex: number, rowHeight: number): HTMLDivElement {
-    const rowHeaderCellElement = document.createElement("div");
-    rowHeaderCellElement.classList.add("cell");
-    rowHeaderCellElement.style.height = `${rowHeight}px`;
-    rowHeaderCellElement.textContent = String(rowIndex + 1);
-    rowHeaderCellElement.style.display = "none";
-
-    return rowHeaderCellElement;
-  }
-
-  private getCellElement(rowIndex: number, column: Column, columnIndex: number, previousCellsWidth: number): HTMLDivElement {
-    const cellElement = document.createElement("div");
-    cellElement.classList.add("cell");
-    cellElement.setAttribute('data-row', `${rowIndex}`);
-    cellElement.setAttribute('data-column', `${columnIndex}`);
-    cellElement.style.width = `${column.width}px`;
-    cellElement.style.transform = `translateX(${previousCellsWidth}px)`;
-    cellElement.textContent = this.cells.get(`${rowIndex}_${columnIndex}`)?.calculatedValue ?? '';
-
-    return cellElement;
-  }
-
   private createSentinelElements(): void {
-    if (!this.tableEl) {
+    if (!this.itemEl) {
       return;
     }
 
     this.topSentinel = document.createElement('div');
     this.topSentinel.classList.add("sentinel");
     this.topSentinel.style.top = '0';
-    this.tableEl.appendChild(this.topSentinel);
+    this.itemEl.appendChild(this.topSentinel);
 
     this.bottomSentinel = document.createElement('div');
     this.bottomSentinel.classList.add("sentinel");
     this.bottomSentinel.style.bottom = '0';
-    this.tableEl.appendChild(this.bottomSentinel);
+    this.itemEl.appendChild(this.bottomSentinel);
 
     this.observer = new IntersectionObserver(this.renderVisibleRows.bind(this), {
       root: this.container,
@@ -191,7 +69,7 @@ export default class VirtualScroll {
       const scrollTop = this.container.scrollTop;
       const newStartIndex = Math.floor(scrollTop / this.rowHeight);
       const isLessFirstRowIndex = newStartIndex < this.startIndex;
-      const isMoreLastRowIndex = newStartIndex > this.endIndex;
+      const isMoreLastRowIndex = newStartIndex > this.endIndex - this.availableRowsCount;
 
       if (isLessFirstRowIndex || isMoreLastRowIndex) {
         this.renderVisibleRows();
@@ -202,7 +80,7 @@ export default class VirtualScroll {
   }
 
   private renderVisibleRows(): void {
-    if (!this.tableContentEl || !this.rowHeadersContainerElement) {
+    if (!this.itemContentEl || !this.table) {
       return;
     }
 
@@ -214,50 +92,21 @@ export default class VirtualScroll {
       this.startIndex + this.visibleRowsCount + this.rowsBuffer
     );
 
-    this.rowHeadersContainerElement.style.transform = `translateY(${this.startIndex * this.rowHeight}px)`;
-    this.tableContentEl.style.transform = `translateY(${this.startIndex * this.rowHeight}px)`;
+    this.table.rowHeadersContainerElement.style.transform = `translateY(${this.startIndex * this.rowHeight}px)`;
+    this.itemContentEl.style.transform = `translateY(${this.startIndex * this.rowHeight}px)`;
 
-    this.hideRows();
+    this.table.hideRows();
 
     let contentIndex = 0;
     let previousRowsHeight = 0;
 
     for (let rowIndex = this.startIndex; rowIndex <= this.endIndex; rowIndex += 1) {
-      this.renderRow(rowIndex, contentIndex, previousRowsHeight);
+      this.table.renderRow(rowIndex, contentIndex, previousRowsHeight);
       previousRowsHeight += this.rowHeight;
       contentIndex += 1;
     }
 
     this.updateSentinelsPosition();
-  }
-
-  hideRows(): void {
-    for (const [index, rowElement] of this.tableRowElements.entries()) {
-      rowElement.style.display = "none";
-      this.tableRowHeaderCellElements[index].style.display = "none";
-    }
-  }
-
-  private renderRow(rowIndex: number, rowContentBlockIndex: number, previousRowsHeight: number): void {
-    if (!this.tableContentEl) {
-      return;
-    }
-
-    const rowHeaderCellElement = this.tableRowHeaderCellElements[rowContentBlockIndex];
-    rowHeaderCellElement.style.transform = `translateY(${previousRowsHeight + this.tableHeaderHeightPx}px)`;
-    rowHeaderCellElement.style.display = "flex";
-    rowHeaderCellElement.textContent = `${rowIndex + 1}`;
-
-    const tableRowEl = this.tableRowElements[rowContentBlockIndex];
-    tableRowEl.setAttribute("data-row-index", `${rowIndex}`);
-    tableRowEl.setAttribute("data-row-array-index", `${rowContentBlockIndex}`);
-    tableRowEl.style.transform = `translateY(${previousRowsHeight}px)`;
-    tableRowEl.style.display = 'flex';
-
-    tableRowEl.querySelectorAll('.cell').forEach((cellElement, cellIndex) => {
-      cellElement.setAttribute('data-row', `${rowIndex}`);
-      cellElement.textContent = this.cells.get(`${rowIndex}_${cellIndex}`)?.calculatedValue ?? '';
-    });
   }
 
   private updateSentinelsPosition(): void {
